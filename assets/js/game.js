1bce0c48a40aaa3178ace3d8a3e645e878fc6f6b
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-04-25 14:25:09 -0400

    add fight var with no user input

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,51 @@
+// player variables
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("Our robot's name is " + playerName)
+//enemy variables
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-function fight() {
-    window.alert("The fight has begun!");
+// functions
+var fight = function() {
+    // alert that the round has started
+    window.alert("Welcome to Robot Gladiators!");
+
+    // subtract player attack from enemy health and update enemy health
+    enemyHealth = enemyHealth - playerAttack;
+
+    // log a message showing it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " +  enemyName + " now has " + enemyHealth + " health remaining."
+    )
+
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    // subtract enemy attack from player health and update player health
+    playerHealth = playerHealth - enemyAttack;
+
+    // log a message showing it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    )
+
+    // check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 }
 
-// fight();
-
-// comments syntax in js
-
-/* wrapped comments have the 
-same or similar syntax
-as CSS does */
+fight();
