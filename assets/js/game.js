b34f4a902f4c409248e63b1b54d15f1c7bd6b235
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-04-25 13:54:26 -0400

    add var playerName, comments, console.logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+
+console.log("Our robot's name is " + playerName)
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
+// comments syntax in js
+
+/* wrapped comments have the 
+same or similar syntax
+as CSS does */
